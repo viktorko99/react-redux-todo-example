37db601be9685f7c7167a273e528b0999b8a8eca
Valaštín Viktor <valastin@domain.local>
2017-06-13 11:29:07 +0200

    Welcome page was rebuilt

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react'
 import { Button, Jumbotron } from 'react-bootstrap';
 
-export default function WelcomePage() {
-  const divStyle = {
-    background: "#eee",
-    padding: "20px",
-    margin: "20px"
-  };
-
-    return (
-      <Jumbotron >
-        <div style={divStyle}>
-          <h1>Todo-list, refined!</h1>
-          <p>Enjoy the beautiness of React and Redux </p>
-          <p>Click on button to learn more</p>
-          <p><Button bsStyle="success">Learn more</Button></p>
-        </div>
-      </Jumbotron>
-    )
+const divStyle = {
+  background: "#eee",
+  padding: "20px",
+  margin: "20px"
 }
+
+class WelcomePage extends Component {
+  constructor(props) {
+    super(props);
+
+      this.handleAdressChange = this.handleAdressChange.bind(this);
+  }
+
+  handleAdressChange() {
+    this.props.history.push('/todo');
+  }
+
+  render() {
+    return (
+        <Jumbotron >
+          <div style={divStyle}>
+            <h1>Todo-list, refined!</h1>
+            <p>Enjoy the beautiness of React and Redux </p>
+            <p>Click on button to learn more</p>
+            <p><Button bsStyle="success" onClick={this.handleAdressChange}>Learn more</Button></p>
+          </div>
+        </Jumbotron>
+    );
+  }
+}
+
+export default WelcomePage;
