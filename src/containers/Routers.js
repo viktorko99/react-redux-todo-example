a142211c42c96dfb402f3302f3129949680f0f27
Valaštín Viktor <valastin@domain.local>
2017-06-20 13:19:17 +0200

    Code refactor

--- src/containers/Routers.js
+++ src/containers/Routers.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
 import TodoLayout from '../components/Todo/TodoLayout';
 import NotFound from '../components/ErrorPages/NotFound';
 import WelcomePage from '../containers/WelcomePage';
@@ -11,6 +12,18 @@ class Routers extends Component {
   render() {
     return (
       <div>
+        <Link to='/'>default</Link>
+        {' '}
+        <Link to='/todo'>todos</Link>
+        {' '}
+        <Link to='/notfound'>not found</Link>
+        {' '}
+        <Link to='/login'>login</Link>
+        {' '}
+        <Link to='/profile'>profile</Link>
+        {' '}
+        <Link to='/register'>register</Link>
+
         <Switch>
           <Route path='/' exact component={WelcomePage}/>
           <Route path='/todo' component={TodoLayout}/>
